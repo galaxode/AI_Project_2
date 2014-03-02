b41fe8cdc1ff485fe558823741c6f5ae61f21488
galaxode <andrew.cookro@gmail.com>
2014-03-01 22:58:12 -0500

    Got cat to turn and animate running but not move
    If you go into animator and change the order of "run" and "walk" it will
    use the bottom one for default. The "walk" has a problem though. It
    stops after only a couple seconds and you have to press key again.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float turnSmoothing = 15f;
+	public float speedDampTime = 0.1f;
+
+	private Animator anim;
+	private HashIDs hash;
+
+
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+		hash = GameObject.FindGameObjectWithTag("GameController").GetComponent<HashIDs>();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		MovementManagement(h, v);
+
+	}
+
+	void MovementManagement(float horizontal, float vertical)
+	{
+		if (horizontal != 0f || vertical != 0f)
+		{
+			Rotating(horizontal, vertical);
+			anim.SetFloat(hash.speedFloat, 5.5f, speedDampTime, Time.deltaTime);
+		}
+		else
+		{
+			anim.SetFloat(hash.speedFloat, 0);
+		}
+	}
+
+	void Rotating(float horizontal, float vertical)
+	{
+		Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
+		Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+		Quaternion newRotation = Quaternion.Lerp(rigidbody.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+		rigidbody.MoveRotation(newRotation);
+
+	}
+}
---
+++ Assets/Scripts/HashIDs.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HashIDs : MonoBehaviour 
+{
+	public int locomotionState;
+	public int speedFloat;
+	public int angularSpeedFloat;
+	public int openBool;
+
+	void Awake()
+	{
+		locomotionState = Animator.StringToHash("Base Layer.Locomotion");
+		speedFloat = Animator.StringToHash("Speed");
+		angularSpeedFloat = Animator.StringToHash("AngularSpeed");
+		openBool = Animator.StringToHash("OpenBool");
+	}
+
+
+}
