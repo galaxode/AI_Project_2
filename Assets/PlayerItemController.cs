ccc4df329b246c1ad72f30dc3bdf3cf2f5b7fefa
galaxode <andrew.cookro@gmail.com>
2014-03-05 21:23:09 -0500

    Added Collectible Items
    Added collectible word gems, and food bowls.  Added GUI text showing
    counts of word gems and food. Also added GUI text showing which word was
    collected.  Words collected are based on number of word gems collected
    right now. Not on a specific gem.

---
+++ Assets/PlayerItemController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerItemController : MonoBehaviour 
+{
+	private int wordCount;
+	public string[] wordPrize = new string[] {"Ardent", "Enigma", "Fastidious", "Decipher" };
+	public GUIText wordCountText;
+	public GUIText wordText;
+
+	private int foodCount;
+	public GUIText foodCountText;
+
+	void Start()
+	{
+		wordCount = 0;
+		setWordCountText();
+		setWordText();
+		foodCount = 0;
+		setFoodCountText();
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "WordGem") //make word gems disappear, increment counter, set text
+		{
+			other.gameObject.SetActive(false);
+			wordCount++;
+			setWordCountText ();
+			setWordText();
+		}
+		if (other.gameObject.tag == "Food") //make food disappear, increment counter, set text
+		{
+			other.gameObject.SetActive (false);
+			foodCount++;
+			setFoodCountText();
+
+		}
+	}
+
+	void setWordCountText()
+	{
+		wordCountText.text = "Words: " + wordCount.ToString();
+	}
+
+	void setWordText()
+	{	
+		if (wordCount < 1) 
+		{
+			wordText.text = "No Words";
+		} 
+		else 
+		{
+			for (int i = 0; i < wordCount; i++) 
+			{
+				wordText.text = wordPrize [i];
+			}
+		}
+	}
+
+	void setFoodCountText()
+	{
+		foodCountText.text = "Food: " + foodCount.ToString();
+	}
+}
