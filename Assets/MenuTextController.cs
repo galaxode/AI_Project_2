1f4e99141b83cd71aa50d2d1f012f75bf0ab1bee
galaxode <andrew.cookro@gmail.com>
2014-03-07 23:29:00 -0500

    Added Menu Screen
    Added menu screen with start game and quit buttons. Also added scenes to
    build settings so that menu screen is 0, grassy is 1, and space (level
    test) is 2

---
+++ Assets/MenuTextController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuTextController : MonoBehaviour 
+{
+
+	public bool isQuitButton = false;
+
+	//change color of text when you hover mouse over text
+	void OnMouseEnter()
+	{
+		renderer.material.color = Color.cyan;
+	}
+
+	//return to white color after mouse is not hovering over text
+	void OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseUp()
+	{
+		if (isQuitButton) 
+		{
+			Application.Quit(); 		//quit game
+		} 
+		else 
+		{
+			Application.LoadLevel(1); 	//load level
+		}
+	}
+}
