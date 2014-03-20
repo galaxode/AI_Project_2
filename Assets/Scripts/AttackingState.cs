77b43f44dc1242aea05e1b6d6b5fca0bcf60f28c
Omer Useche <madrusec@gmail.com>
2014-03-19 23:08:46 -0400

    Added attacking state for NPC. Cat can throw too

---
+++ Assets/Scripts/AttackingState.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackingState : MonoBehaviour {
+
+	public static int amountOfAmmo = 10;
+	public float timeInterval = 2f;
+	public Rigidbody npcAmmo;
+	public float shootingForce = 2f;
+
+	private float timer;
+	private Vector3 playerPosition;
+	private Vector3 shootingDirection;
+	private GameObject hand;
+
+	void Awake()
+	{
+		timer = timeInterval;
+		hand = GameObject.FindGameObjectWithTag("Hand");  
+	}
+
+
+
+	public void AttackPlayer()
+	{
+
+		timer -= Time.deltaTime;
+
+		//We set this up to throw ammo every two seconds or those sepcifed for timeInterval
+		if(timer <= 0 && amountOfAmmo > 0)
+		{
+			playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+			shootingDirection = playerPosition - hand.transform.position;
+			shootingDirection.Normalize();
+			//shootingDirection.y = 2f;
+
+			//The below creates new instances of the amo and positiones it at the "hand" position. 
+			Rigidbody cloneAmmo;
+
+			cloneAmmo = Instantiate(npcAmmo, hand.transform.position, hand.transform.rotation) as Rigidbody;
+			cloneAmmo.rigidbody.AddForce(shootingDirection * shootingForce);	//Thow it at player 
+
+			timer = timeInterval;		//Reset timer before next throw
+			amountOfAmmo--;				// Reduce the ammount of ammo available 
+		}
+	}
+
+
+	//Allows other classes to determine if the NPC is out of ammo
+	public bool OutOfAmmo()
+	{
+		if(amountOfAmmo <= 0) 
+			return true;
+		else
+			return false;
+	}
+}
---
+++ Assets/Scripts/PlayerThrowingController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerThrowingController : MonoBehaviour {
+
+	private int amountOfAmmo = 5;
+
+	public float shootingForce = 300;
+	public Transform ammoSpawner;
+	public Rigidbody ammo;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButtonDown("Fire1") && amountOfAmmo > 0)
+		{
+			Rigidbody cloneAmmo;
+			
+			cloneAmmo = Instantiate(ammo, ammoSpawner.position, ammoSpawner.rotation) as Rigidbody;
+			cloneAmmo.rigidbody.AddForce(rigidbody.velocity + ammoSpawner.forward * shootingForce);	//Thow it at player 
+		
+			amountOfAmmo--;				// Reduce the ammount of ammo available 
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Ammo")
+		{
+			Destroy(col.gameObject);
+
+			amountOfAmmo++;
+		}
+	}
+}
