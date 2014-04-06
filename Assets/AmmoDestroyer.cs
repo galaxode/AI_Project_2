5dbf0e075d50e958135e3fb153a50dd7d687f440
galaxode <andrew.cookro@gmail.com>
2014-04-06 00:09:21 -0400

    Small Additions
    Turned Ammo prefab into EnemyAmmo and PlayerAmmo.  NPC1 will deactivate
    if health falls to zero in space maze. Experimented with camera. There
    is a WoW style camera you can try in Space Maze, but it does weird
    things. I scaled down models in Space Maze so that they are around the
    same size as in Grassy Field.  I still have not added NPC1Health, etc.
    to Grassy Field.  There are no WordGems there yet either.

---
+++ Assets/AmmoDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmoDestroyer : MonoBehaviour 
+{
+
+	// Destroy ammo game object after 4 seconds
+	void Start () 
+	{
+		Destroy(gameObject, 4);
+	}
+	
+
+}
